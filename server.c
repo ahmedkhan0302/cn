8fa9c5ae370531c1a8cbd2fdc6f12a6b1ee97b74
Ahmed Khan <68923785+ahmedkhan0302@users.noreply.github.com>
2025-07-01 06:40:22 +0530

    Update server.c

--- server.c
+++ server.c
@@ -35,3 +35,40 @@ int main() {
     close(server_socket);
     return 0;
 }
+/*#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    int arr[10], key, index;
+
+    printf("Enter 10 integers:\n");
+    for (int i = 0; i < 10; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter the number to search: ");
+    scanf("%d", &key);
+
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+
+    connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    write(client_socket, arr, sizeof(arr));   // send array
+    write(client_socket, &key, sizeof(key));  // send key
+
+    read(client_socket, &index, sizeof(index)); // receive result
+
+    if (index == -1)
+        printf("Element not found in the array.\n");
+    else
+        printf("Element found at index: %d\n", index);
+
+    close(client_socket);
+    return 0;
+}
+*/
