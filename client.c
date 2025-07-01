bbe4486f2de161e14e9ec1241b5f04368409b63c
Ahmed Khan <68923785+ahmedkhan0302@users.noreply.github.com>
2025-07-01 06:38:56 +0530

    Update client.c

--- client.c
+++ client.c
@@ -34,6 +34,44 @@ int main() {
     close(client_socket);
     return 0;
 }
+/*#include <stdio.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key)
+            return i; // return index
+    }
+    return -1; // not found
+}
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr;
+    int arr[10], key, index;
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    listen(server_socket, 1);
+    client_socket = accept(server_socket, NULL, NULL);
+
+    read(client_socket, arr, sizeof(arr));   // read 10 integers
+    read(client_socket, &key, sizeof(key));  // read key to search
+
+    index = linearSearch(arr, 10, key);
+
+    write(client_socket, &index, sizeof(index)); // send result
+
+    close(client_socket);
+    close(server_socket);
+    return 0;
+}*/
+
 //gcc echo_server.c -o server
 //gcc echo_client.c -o client
 
